e02ff2accdeadbb15c4fc856310d7ac7d473ce27
unknown <ruktianibnu95@gmail.com>
2020-11-14 22:46:39 +0700

    + design report

--- src/pages/Report.js
+++ src/pages/Report.js
@@ -8,29 +8,32 @@ import { BASE_URL } from '../helpers/global';
 import moment from 'moment';
 import { Alert } from 'react-native';
 import { setLoading } from '../store/actionCreator';
+import CheckBox from '@react-native-community/checkbox';
+import { DataTable } from 'react-native-paper';
 
 
 export default function Report() {
   const [showAwal, setShowAwal] = useState(false);
   const [showAkhir, setShowAkhir] = useState(false);
-  const [kategori, setKategori] = useState([]);
+  const [layanan, setLayanan] = useState([]);
   const [satker, setSatker] = useState([]);
-  const [kategoriChild, setKategoriChild] = useState([]);
-  const [kategoriDropdown, setKategoriDropdown] = useState([]);
-  const [selectedKategori, setSelectedKategori] = useState('');
+  const [dataTable, setDataTable] = useState([]);
+  const [layananDropdown, setLayananDropdown] = useState([]);
+  const [selectedLayanan, setSelectedLayanan] = useState('');
   const [satkerDropdown, setSatkerDropdown] = useState([]);
   const [selectedSatker, setSelectedSatker] = useState('');
   const [selectedTanggalAwal, setSelectedTanggalAwal] = useState(new Date(Date.now()));
   const [selectedTanggalAkhir, setSelectedTanggalAkhir] = useState(new Date(Date.now()));
   const accessToken = useSelector((state) => state.accessToken);
+  const [toggleCheckBox, setToggleCheckBox] = useState(false)
   const dispatch = useDispatch();
 
   useEffect(() => {
-    GetKategoriPNBP();
+    GetLayanan();
     GetSatker();
   }, []);
 
-  const GetKategoriPNBP = async () => {
+  const GetLayanan = async () => {
     try {
       const headers = {
         Authorization: accessToken,
@@ -42,14 +45,14 @@ export default function Report() {
       dispatch(setLoading(false));
       const { data, status } = response;
       if (status === 200) {
-        const kategori = data.data;
-        const kategoriArr = [];
-        //console.log(kategori);
-        for (const iterator of kategori) {
-          kategoriArr.push(iterator);
+        const layanan = data.data;
+        const layananArr = [];
+        //console.log(layanan);
+        for (const iterator of layanan) {
+          layananArr.push(iterator);
         }
-        setKategoriDropdown(kategoriArr);
-        setKategori(data.data);
+        setLayananDropdown(layananArr);
+        setLayanan(data.data);
       }
     } catch (error) {
       //console.log(error);
@@ -84,90 +87,157 @@ export default function Report() {
     }
   };
 
+  const body = {
+    cekbox: toggleCheckBox,
+    id_jenis: selectedLayanan,
+    id_satker: selectedSatker,
+    tanggal_awal: selectedTanggalAwal,
+    tanggal_akhir: selectedTanggalAkhir,
+  };
+
+  const headers = {
+    Authorization: accessToken,
+  };
+
+  const GetFilter = async () => {
+    try {
+      const response = await Axios.get(`${BASE_URL}/resources/pnbp-kategori`, {
+        headers,
+        body,
+      });
+
+      const { status, data } = response;
+      if (status === 200) {
+        setDataTable(data.data);
+      }
+      else {
+        console.log("data kosong")
+      }
+    }
+    catch (errr) {
+
+    }
+  }
+
   return (
     <>
-        <Container>
-          <Row>
-            <InputContainer onPress={() => setShowAwal(!showAwal)}>
-              <>
-                <Text>Tanggal</Text>
-                <DateInput
-                  editable={false}
-                  value={moment(selectedTanggalAwal).format('YYYY-MM-DD')}
-                />
-              </>
-            </InputContainer>
-            {showAwal && (
-              <DateTimePicker
-                value={selectedTanggalAwal}
-                mode="date"
-                display="calendar"
-                onChange={(event, selectedDate) => {
-                  setShowAwal(!showAwal);
-                  setSelectedTanggalAwal(selectedDate || selectedTanggalAwal);
-                }}
-                on
+      <Container>
+        <Row>
+          <InputContainer onPress={() => setShowAwal(!showAwal)}>
+            <>
+              <Text>Tanggal</Text>
+              <DateInput
+                editable={false}
+                value={moment(selectedTanggalAwal).format('YYYY-MM-DD')}
               />
-            )}
-            <InputContainer onPress={() => setShowAkhir(!showAkhir)}>
-              <>
-                <Text></Text>
-                <DateInput
-                  editable={false}
-                  value={moment(selectedTanggalAkhir).format('YYYY-MM-DD')}
-                />
-              </>
-            </InputContainer>
-            {showAkhir && (
-              <DateTimePicker
-                value={selectedTanggalAkhir}
-                mode="date"
-                display="calendar"
-                onChange={(event, selectedDate) => {
-                  setShowAkhir(!showAkhir);
-                  setSelectedTanggalAkhir(selectedDate || selectedTanggalAkhir);
-                }}
-                on
+            </>
+          </InputContainer>
+          {showAwal && (
+            <DateTimePicker
+              value={selectedTanggalAwal}
+              mode="date"
+              display="calendar"
+              onChange={(event, selectedDate) => {
+                setShowAwal(!showAwal);
+                setSelectedTanggalAwal(selectedDate || selectedTanggalAwal);
+              }}
+              on
+            />
+          )}
+          <InputContainer onPress={() => setShowAkhir(!showAkhir)}>
+            <>
+              <Text></Text>
+              <DateInput
+                editable={false}
+                value={moment(selectedTanggalAkhir).format('YYYY-MM-DD')}
               />
-            )}
-          </Row>
-          <Text>Jenis Layanan</Text>
-          <PickerContainer>
-            <Picker
-              selectedValue={selectedKategori}
-              onValueChange={(itemValue, _) => setSelectedKategori(itemValue)}>
-              <Picker.Item value="all" label="SEMUA JENIS LAYANAN" />
-              {kategoriDropdown.map((item) => (
-                <Picker.Item
-                  key={item.id}
-                  label={item.nama_layanan}
-                  value={item.id}
-                />
-              ))}
-            </Picker>
-          </PickerContainer>
-          <Text>Satuan Kerja</Text>
-          <PickerContainer>
-            <Picker
-              selectedValue={selectedSatker}
-              onValueChange={(itemValue, _) => setSelectedSatker(itemValue)}>
-              <Picker.Item value="all" label="SEMUA SATUAN KERJA" />
-              {satkerDropdown.map((item, index) => (
-                <Picker.Item
-                  key={index}
-                  label={item.nama_kantor}
-                  value={item.id_kantor}
-                />
-              ))}
-            </Picker>
-          </PickerContainer>
-          <BottonContainer>
-              <ButtonNext>
-                <ButtonLabel>Cari</ButtonLabel>
-              </ButtonNext>
-            </BottonContainer>
-        </Container>
-      </>
+            </>
+          </InputContainer>
+          {showAkhir && (
+            <DateTimePicker
+              value={selectedTanggalAkhir}
+              mode="date"
+              display="calendar"
+              onChange={(event, selectedDate) => {
+                setShowAkhir(!showAkhir);
+                setSelectedTanggalAkhir(selectedDate || selectedTanggalAkhir);
+              }}
+              on
+            />
+          )}
+        </Row>
+        <Row>
+          <CheckBox
+            disabled={false}
+            value={toggleCheckBox}
+            onValueChange={(newValue) => setToggleCheckBox(newValue)}
+          />
+          <Text style={{ marginTop: 5 }}>Perbulan Pertahun</Text>
+        </Row>
+        <Text>Jenis Layanan</Text>
+        <PickerContainer>
+          <Picker
+            selectedValue={selectedLayanan}
+            onValueChange={(itemValue, _) => setSelectedLayanan(itemValue)}>
+            <Picker.Item value="all" label="SEMUA JENIS LAYANAN" />
+            {layananDropdown.map((item) => (
+              <Picker.Item
+                key={item.id}
+                label={item.nama_layanan}
+                value={item.id}
+              />
+            ))}
+          </Picker>
+        </PickerContainer>
+        <Text>Satuan Kerja</Text>
+        <PickerContainer>
+          <Picker
+            selectedValue={selectedSatker}
+            onValueChange={(itemValue, _) => setSelectedSatker(itemValue)}>
+            <Picker.Item value="all" label="SEMUA SATUAN KERJA" />
+            {satkerDropdown.map((item, index) => (
+              <Picker.Item
+                key={index}
+                label={item.nama_kantor}
+                value={item.id_kantor}
+              />
+            ))}
+          </Picker>
+        </PickerContainer>
+        <BottonContainer>
+          <ButtonNext onPress={GetFilter}>
+            <ButtonLabel>Cari</ButtonLabel>
+          </ButtonNext>
+        </BottonContainer>
+        <DataTable style={{marginTop:10}}>
+          <DataTable.Header>
+            <DataTable.Title>Dessert</DataTable.Title>
+            <DataTable.Title numeric>Calories</DataTable.Title>
+            <DataTable.Title numeric>Fat</DataTable.Title>
+          </DataTable.Header>
+
+          <DataTable.Row>
+            <DataTable.Cell>Frozen yogurt</DataTable.Cell>
+            <DataTable.Cell numeric>159</DataTable.Cell>
+            <DataTable.Cell numeric>6.0</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+            <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+            <DataTable.Cell numeric>237</DataTable.Cell>
+            <DataTable.Cell numeric>8.0</DataTable.Cell>
+          </DataTable.Row>
+        </DataTable>
+        <DataTable.Pagination
+          page={1}
+          numberOfPages={3}
+          onPageChange={page => {
+            console.log(page);
+          }}
+          label="oke muncul"
+        />
+    </Container>
+    </>
   );
 }
 
